26f9a69dcf9cc61207edf77fcabc826831c3ff98
Yuxi Fu <yuxifu2004@yahoo.com>
2017-02-21 14:06:51 -0800

    Initial  Commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Angular2heroes
 
+https://angular.io/docs/ts/latest/tutorial/
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0-beta.32.3.
 
 ## Development server
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular2heroes App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Tour of Heroes');
   });
 });
