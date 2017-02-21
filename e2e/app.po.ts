98c3491a3ffdc23ac0feb6ac89c8ddaa116dade5
Angular CLI <angular-cli@angular.io>
2017-02-21 13:38:49 -0800

    chore: initial commit from @angular/cli
                              _                      _ _
       __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)
      / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |
     | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |
      \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|
                  |___/

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class Angular2heroesPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
